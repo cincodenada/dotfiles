abd974ccf794cb503d788593c56f6c3f32aa2f3c
Joel Bradshaw <cincodenada@gmail.com>
2014-06-07 12:37:52 -0700

    Some accumulated changes, mostly vim stuff

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,15 +3,16 @@
 #   `brew install coreutils`
 if $(gls &>/dev/null)
 then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la="gls -A --color"
+  alias ls="gls -F --color=auto"
+  alias l="gls -lAh --color=auto"
+  alias ll="gls -l --color=auto"
+  alias la="gls -A --color=auto"
 else
-  alias ls="ls -F --color"
-  alias l="ls -lAh --color"
-  alias ll="ls -l --color"
-  alias la="ls -A --color"
+  alias ls="ls -F --color=auto"
+  alias l="ls -lAh --color=auto"
+  alias ll="ls -l --color=auto"
+  alias la="ls -A --color=auto"
 fi
 
 alias unescape='perl -e "use URI::Escape; print uri_unescape(<>);"'
+alias curscrot="ls --color=no -t1 ~/*scrot.png | head -n1"
