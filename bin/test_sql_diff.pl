3dcd092f1832110cd816c6a1744f826e856f81e2
Joel Bradshaw <joel.bradshaw@cedexis.com>
2018-11-13 17:23:57 -0800

    Update sql diff for Catch2

--- bin/test_sql_diff.pl
+++ bin/test_sql_diff.pl
@@ -4,22 +4,31 @@ open(my $out_actual, '>', 'actual.sql');
 
 my $testname, $outfile;
 while(<STDIN>) {
-  if(/RUN.* ([\w\.]+)$/) {
+  if(/^\/ (.*) \-+$/) {
+    if($testname and $outfile) {
+      print $outfile "-- $testname\n";
+    }
     $testname = $1;
   }
-  if(/Expected/) {
-    $outfile = $out_actual; 
-  }
-  if(/To be equal to/) {
+  if(/^with expansion:/) {
     $outfile = $out_expected;
-  }
-  if(/Which is: "(.*)"$/) {
-    print $outfile "-- $testname\n";
-    print $outfile "$1;\n";
+  } elsif(/^  \=\=$/) {
+    $outfile = $out_actual;
+  } else {
+    if($outfile and $testname) {
+      if(/^  "(.*)/) {
+        print $outfile "$1";
+      } elsif(/^  (.*)"$/) {
+        print $outfile "$1;";
+        $outfile = undef;
+      } else {
+        print $outfile $_;
+      }
+    }
   }
 }
 
-foreach my $fname (['expected','actual']) {
+foreach my $fname ('expected','actual') {
   system("sqlformat --reindent --wrap_after 80 $fname.sql | sponge $fname.sql");
 }
 
