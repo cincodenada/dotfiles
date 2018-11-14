640de22a0f30e65e8b92ddabfd687c931bf9c742
Joel Bradshaw <joel.bradshaw@cedexis.com>
2018-11-13 17:26:37 -0800

    Bind more keys in zsh to avoid weird vim mode things

--- zsh/setkeys.zsh
+++ zsh/setkeys.zsh
@@ -1,8 +1,3 @@
-[[ -z "$terminfo[kdch1]" ]] || bindkey "$terminfo[kdch1]" delete-char
-[[ -z "$terminfo[khome]" ]] || bindkey "$terminfo[khome]" beginning-of-line
-[[ -z "$terminfo[kend]" ]] || bindkey "$terminfo[kend]" end-of-line
-[[ -z "$terminfo[kich1]" ]] || bindkey "$terminfo[kich1]" overwrite-mode
-
 bindkey -v
 export KEYTIMEOUT=1 # Speed up mode switching
 
@@ -17,3 +12,10 @@ bindkey '^h' backward-delete-char
 
 bindkey '^P' up-history
 bindkey '^N' down-history
+
+[[ -z "$terminfo[kdch1]" ]] || bindkey "$terminfo[kdch1]" delete-char
+[[ -z "$terminfo[khome]" ]] || bindkey "$terminfo[khome]" beginning-of-line
+[[ -z "$terminfo[kend]" ]] || bindkey "$terminfo[kend]" end-of-line
+[[ -z "$terminfo[kich1]" ]] || bindkey "$terminfo[kich1]" overwrite-mode
+[[ -z "$terminfo[knp]" ]] || bindkey "$terminfo[knp]" down-history
+[[ -z "$terminfo[kpp]" ]] || bindkey "$terminfo[kpp]" up-history
