86ec5439501a6019615240256664bbaab4bd3bbc
Ell Bradshaw <joelbr@tailormed.co>
2022-12-14 15:15:54 -0800

    Try to speed the auto NVM up more

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -11,13 +11,25 @@ load-nvmrc() {
   local file="${funcfiletrace[1]%:*}"
   [[ "$file" == *"_cd" ]] && return
 
+  # Moving one directory at a time, we don't need to re-check the whole tree
+  if [ "${OLDPWD%\/*}" = "$PWD" ]; then
+    [ -f "$OLDPWD/.nvmrc" ] || return
+  elif [ "${PWD%\/*}" = "$OLDPWD" ]; then
+    [ -f "$PWD/.nvmrc" ] || return
+  fi
+
   local nvmrc_path="$(nvm_find_nvmrc)"
   local node_version=${${NVM_BIN##*node\/}%\/bin}
   if [ -z "$node_version" ]; then
     node_version = $(nvm version)
   fi
 
+  # Exit if we didn't change nvmrc's
+  [ "$nvmrc_path" = "$LOAD_NVMRC_PREV_PATH" ] && return
+  export LOAD_NVMRC_PREV_PATH=$nvmrc_path
+
   if [ -n "$nvmrc_path" ]; then
+    echo "Checking nvm version..."
     local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
   
     if [ "$nvmrc_node_version" = "N/A" ]; then
