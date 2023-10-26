70040ceca54c65b45863e1b5c069e4d8394a2f75
Joel Bradshaw <joel@cincodenada.com>
2023-10-26 12:40:06 -0700

    Updates from t470

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$HOME/bin:$HOME/bin/*:$HOME/.local/bin:$HOME/.cargo/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$HOME/bin:$HOME/bin/*:$HOME/.local/bin:$HOME/.nix-profile/bin:$HOME/.cargo/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
 # Load /etc/profile to pick up profile.d, ugh???
